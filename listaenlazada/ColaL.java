ed7875926a67a740a6f11958746e60651b34253f
Axel Espinosa <axeldavid45@gmail.com>
2019-10-14 11:06:38 -0500

    Creando la implementacion de listaenlazada

---
+++ listaenlazada/ColaL.java
@@ -0,0 +1,19 @@
+package listaenlazada;
+
+public class ColaL extends LinkedList {
+    public Nodo pop() {
+         Nodo aux = this.start;
+        if (this.isEmpty())
+            return null;
+        else {
+            if (aux == this.start) {
+                this.start = this.start.getNext();
+                if (this.start == null)
+                    this.end = null;
+                else
+                    this.start.setPrevious(null);
+            }
+        }
+        return aux;
+    }
+}
